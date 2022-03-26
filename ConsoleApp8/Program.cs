05a2bf61d62e1c1c89e4e3f052e5f4fb758b2f05
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-03-26 15:49:32 +0400

    выполнил упражнение с записью и вызовом массива в 1 строку

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetSuit(Suits.Wands));
+			Console.WriteLine(GetSuit(Suits.Coins));
+			Console.WriteLine(GetSuit(Suits.Cups));
+			Console.WriteLine(GetSuit(Suits.Swords));
+			Console.ReadKey();
+		}
+
+		enum Suits
+		{
+			Wands,
+			Coins,
+			Cups,
+			Swords
+		}
+
+		private static string GetSuit(Suits suit)
+		{
+			//решение в 2 строки
+			//var array = new[] { "жезлов", "монет", "кубков", "мечей" };
+			//return array[(int)suit];
+			
+			//решение в 1 строку
+			return new[] { "жезлов", "монет", "кубков", "мечей" }[(int)suit];
+		}
+	}
+}
