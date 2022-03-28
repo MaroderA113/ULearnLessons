f12a841c405ead4f298ba33f7f2caa9986820f1a
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-03-28 23:31:15 +0400

    разбирал чужое решение задания с записями вида <name>:<email>

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> contacts = new List<string>();
+			contacts.Add("Ваня:v@mail.ru");
+			contacts.Add("Ваня:ivan@grozniy.ru");
+			contacts.Add("Вася:vasiliy@gmail.com");
+      Console.WriteLine(OptimizeContacts(contacts));
+
+      Console.ReadKey();
+		}
+
+		private static Dictionary<string, List<string>> OptimizeContacts(List<string> contacts)
+		{
+			var dictionary = new Dictionary<string, List<string>>();
+			foreach (var contact in contacts)
+			{
+				var name = contact.Substring(0, 2).Replace(":", "");
+				Console.WriteLine(name);
+				if (!dictionary.ContainsKey(name))
+					dictionary[name] = new List<string>();
+				dictionary[name].Add(contact);
+			}
+			return dictionary;
+		}
+	}
+}
