a22f9dfb940de8a4ae7f319a088a47ae87879092
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-03 13:33:41 +0400

    задание из лекции рабирал про stringbuilder и таймер

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp13
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var watch = new Stopwatch();
+			watch.Start();
+			WrongConcatenation();
+			watch.Stop();
+			Console.WriteLine(watch.ElapsedMilliseconds);
+
+			watch = new Stopwatch();
+			watch.Start();
+			RightConcatenation();
+			watch.Stop();
+			Console.WriteLine(watch.ElapsedMilliseconds);
+
+			Console.ReadKey();
+		}
+
+		static void WrongConcatenation()
+		{
+			//Если нам нужно сконкатенировать большое количество строк
+			//то конкатенация "в лоб" потребует очень много памяти в куче, 
+			//и будет работать медленно
+
+			var str = "";
+
+			for (int i = 0; i < 50000; i++)
+			{
+				str += i.ToString() + ", ";
+			}
+		}
+
+		static void RightConcatenation()
+		{
+			//Конкатенация со StringBuilder работает в сотни раз быстрее
+			//Однако, в случае 3-4 строк вы не почувствуете разницы, поэтому 
+			//в этом случае пользоваться StringBuilder не обязательно
+			var builder = new StringBuilder();
+
+			for (int i = 0; i < 50000; i++)
+			{
+				builder.Append(i);
+				builder.Append(", ");
+			}
+		}
+	}
+}
