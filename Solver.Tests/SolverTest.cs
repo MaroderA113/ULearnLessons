e0a78da17b6109e6c90215f182e393f7221f6449
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-05 21:13:37 +0400

    добавил второй тест для класса QuadraticEquationSolver

--- Solver.Tests/SolverTest.cs
+++ Solver.Tests/SolverTest.cs
@@ -5,19 +5,33 @@ namespace Solver.Tests
 	[TestClass]
 	public class SolverTest
 	{
+		
 		[TestMethod]
 		public void OrdinatyEquation()
 		{
-			//Arrange
+			//Arrange - Инициализация значения
 			var a = 1;
 			var b = -3;
 			var c = 2;
-			//Act
+			//Act - Вызов функциональности
 			var result = QuadraticEquationSolver.Solve(a, b, c);
-			//Assert
+			//Assert - Проверка
 			Assert.AreEqual(2,result.Length);
 			Assert.AreEqual(2, result[0]);
 			Assert.AreEqual(1, result[1]);
+		}
+
+		[TestMethod]
+		public void NegativeDiscriminant()
+		{
+			//AArrange - Инициализация значения
+			var a = 1;
+			var b = 1;
+			var c = 1;
+			//Act - Вызов функциональности
+			var result = QuadraticEquationSolver.Solve(a, b, c);
+			//Assert - Проверка
+			Assert.AreEqual(0, result.Length);
 		}
 	}
 }
--- Solver/QuadraticEquationSolver.cs
+++ Solver/QuadraticEquationSolver.cs
@@ -7,10 +7,17 @@ namespace Solver
 		public static double[] Solve(double a, double b, double c)
 		{
 			var discriminant = b * b - 4 * a * c;
-			var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
-			var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
-
-			return new[] { x1, x2 };
+			if (discriminant > 0)
+			{
+				var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+				var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+				return new[] { x1, x2 };
+			}
+			else if (discriminant == 0)
+			{
+				return new[] { -b / (2 * a) };
+			}
+			else return new double[0];
 		}
 	}
 }
