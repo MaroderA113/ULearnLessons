d0798851fb12d10e12257804444f66cba01bc544
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-10 14:28:48 +0400

    пример рекурсии из лекции и разбор задания по исправлению рекурсии

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+	class Program
+	{
+		static void Make (int n)
+		{
+			for (int i = n-1; i >= 0; i--)
+			{
+				Console.Write(i + " ");
+				Make(i);
+			}
+		}
+		
+		static void Main(string[] args)
+		{
+			Make(3);
+		}
+	}
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+	class Program
+	{
+		public static void WriteReversed(char[] items, int startIndex = 0)
+		{
+			if (startIndex == items.Length)
+				return;
+			WriteReversed(items, startIndex + 1);
+			Console.Write(items[startIndex]);
+		}
+
+		static void Main(string[] args)
+		{
+			WriteReversed(new char[6]{ 'a', 'b', 'c', 'd', 'e', 'f'}, 2);
+			Console.ReadKey();
+		}
+	}
+}
