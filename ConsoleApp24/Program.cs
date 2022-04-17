dd8444f196fbdc2d5a9198ceb1d2f18780c3e329
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-17 17:26:04 +0400

    пример из лекции - сортировка слияением

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+	class Program
+	{
+    static int[] temporaryArray;
+
+    static void Merge(int[] array, int start, int middle, int end)
+    {
+      var leftPtr = start;
+      var rightPtr = middle + 1;
+      var length = end - start + 1;
+      for (int i = 0; i < length; i++)
+      {
+        if (rightPtr > end || (leftPtr <= middle && array[leftPtr] < array[rightPtr]))
+        {
+          temporaryArray[i] = array[leftPtr];
+          leftPtr++;
+        }
+        else
+        {
+          temporaryArray[i] = array[rightPtr];
+          rightPtr++;
+        }
+      }
+      for (int i = 0; i < length; i++)
+        array[i + start] = temporaryArray[i];
+    }
+
+    static void MergeSort(int[] array, int start, int end)
+    {
+      if (start == end) return;
+      var middle = (start + end) / 2;
+      MergeSort(array, start, middle);
+      MergeSort(array, middle + 1, end);
+      Merge(array, start, middle, end);
+
+    }
+
+    static void MergeSort(int[] array)
+    {
+      temporaryArray = new int[array.Length];
+      MergeSort(array, 0, array.Length - 1);
+    }
+
+    static Random random = new Random();
+
+    static int[] GenerateArray(int length)
+    {
+      var array = new int[length];
+      for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next();
+      return array;
+    }
+
+    public static void Main()
+    {
+      var array = GenerateArray(10);
+      MergeSort(array);
+      foreach (var e in array)
+        Console.WriteLine(e);
+      Console.ReadKey();
+    }
+  }
+}
