8c3b5c2f1be6b1154523862f2bb443233c96e695
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-25 19:30:22 +0400

    пример из лекции про отличие статических и динаических полей класса

---
+++ ConsoleAppOOP3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppOOP3
+{
+	public class RightTriangle
+	{
+		public double Hypothenuse;
+		public double[] Cathetes;
+
+		public static double AngleBetweenCathetes = Math.PI / 2;
+	}
+
+	public class Program
+	{
+		static void Main()
+		{
+			// Создание объекта-треугольника.
+
+			// RightTriangle — это тип данных, который определяет, какую информацию
+			// и как мы храним в файле о прямоугольном треугольнике
+
+			// firstTriangle — это конкретная область памяти, отформатированная
+			// в соответствии с типом RightTriangle
+			var firstTriangle = new RightTriangle();
+			// Обращение к динамическому полю
+			firstTriangle.Hypothenuse = 5;
+			firstTriangle.Cathetes = new double[2];
+			firstTriangle.Cathetes[0] = 3;
+			firstTriangle.Cathetes[1] = 4;
+
+			//Обращение к статическому полю
+			Console.WriteLine(RightTriangle.AngleBetweenCathetes);
+
+		}
+	}
+}
