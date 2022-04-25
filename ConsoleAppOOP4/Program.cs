1a00abfee528308a9d9ce747e28309bc39536966
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-04-25 21:14:17 +0400

    разбор задания из лекции про методы классов

---
+++ ConsoleAppOOP4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppOOP4
+{
+	class Point
+	{
+		public int X;
+		public int Y;
+
+		public void Print()
+		{
+			Console.WriteLine("{0} {1}", X, Y);
+		}
+
+		public static void PrintPoint(Point point)
+		{
+			Console.WriteLine("{0} {1}", point.X, point.Y);
+		}
+	}
+
+	class Program
+	{
+		public static void Main()
+		{
+			var point = new Point { X = 1, Y = 2 };
+			point.Print();
+			Point.PrintPoint(point);
+		}
+	}
+}
