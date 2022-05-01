8e53df1653e737defd450331a5eb0a11ffb6d4c4
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-05-01 19:06:07 +0400

    пример из лекции по работе с директориями и файлами (классы)

---
+++ ConsoleAppOOP7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppOOP7
+{
+	class Program
+	{
+		static void Main()
+		{
+			foreach (var file in Directory.GetFiles("."))
+				Console.WriteLine(file);
+
+			Console.WriteLine(Directory.GetParent("."));
+
+			var directoryInfo = new DirectoryInfo(".");
+			foreach (var file in directoryInfo.GetFiles())
+				Console.WriteLine(file.Name);
+			directoryInfo = directoryInfo.Parent;
+			Console.WriteLine(directoryInfo.FullName);
+		}
+	}
+}
