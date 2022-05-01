33a557a9611a29c8738b2dfdc016e378f0dd4e3b
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-05-01 19:47:17 +0400

    промеры из лекции про наследование

---
+++ ConsoleAppInher1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleAppInher1
+{
+	class Program
+	{
+		static void Main()
+		{
+			SortIntArray(new int[] { 1, 2, 3 });
+			//SortIntArray(new string[] { "A", "B", "C" }); // так сделать нельзя
+			SortStringArray(new string[] { "A", "B", "C" });
+		}
+
+		static void SortIntArray(int[] array)
+		{
+			for (int i = array.Length - 1; i > 0; i--)
+				for (int j = 1; j <= i; j++)
+					if (array[j] < array[j - 1])
+					{
+						var temp = array[j];
+						array[j] = array[j - 1];
+						array[j - 1] = temp;
+					}
+		}
+
+		static void SortStringArray(string[] array)
+		{
+			for (int i = array.Length - 1; i > 0; i--)
+				for (int j = 1; j <= i; j++)
+					if (array[j].CompareTo(array[j - 1]) < 0)
+					{
+						var temp = array[j];
+						array[j] = array[j - 1];
+						array[j - 1] = temp;
+					}
+		}
+	}
+}
---
+++ ConsoleAppInher2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppInher2
+{
+	class Transport
+	{
+	public double Velocity;
+	public double KilometerPrice;
+	public int Capacity;
+}
+
+class CombustionEngineTransport : Transport
+// это двоеточие обозначает наследование
+{
+	public double EngineVolume;
+	public double EnginePower;
+}
+
+enum ControlType
+{
+	Forward,
+	Backward
+}
+
+class Car : CombustionEngineTransport
+{
+	public ControlType Control;
+}
+
+class Program
+	{
+		static void Main(string[] args)
+		{
+		var c = new Car();
+		c.Control = ControlType.Backward; //это собственное поле класса Car
+		c.EnginePower = 100; // это поле унаследовано от CombustionEngineTransport
+		c.Capacity = 4; // это поле унаследовано от Transport
+	}
+	}
+}
