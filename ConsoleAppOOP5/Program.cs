d9ab4bb75cb5116f3949cd746d55c90b776abc21
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-05-01 18:23:04 +0400

    пример из лекции с методом расширения и решение задания

---
+++ ConsoleAppOOP5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleAppOOP5
+{
+	public class Program
+	{
+		static double MyNextDouble(Random rnd, double min, double max)
+		{
+			return rnd.NextDouble() * (max - min) + min;
+		}
+
+		static void Main()
+		{
+			var rnd = new Random();
+			Console.WriteLine(MyNextDouble(rnd, 10, 20));
+			Console.WriteLine(rnd.NextDouble(10, 20));
+			var array = new int[] { 1, 2, 3, 4, 5 };
+			array.Swap(0, 1);
+			for (int i = 0; i < array.Length; i++)
+				Console.Write(array[i] + "_");
+		}
+	}
+
+	public static class RandomExtensions
+	{
+		public static double NextDouble(this Random rnd, double min, double max)
+		{
+			return rnd.NextDouble() * (max - min) + min;
+		}
+	}
+
+	public static class ArrayExtensions
+	{
+		public static void Swap(this int[] array, int i, int j)
+		{
+			var t = array[i];
+			array[i] = array[j];
+			array[j] = t;
+		}
+	}
+}
---
+++ ConsoleAppOOP6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppOOP6
+{
+	public static class StringExtensions
+	{
+		public static int ToInt(this string arg1)
+		{
+			return int.Parse(arg1);
+		}
+
+		public static int ToInt2(this string arg1) => int.Parse(arg1);
+
+	}
+
+	class Program
+	{
+		
+		public static void Main()
+		{
+			var arg1 = "100500";
+			Console.Write(arg1.ToInt2() + "42".ToInt()); // 100542
+		}
+	}
+}
