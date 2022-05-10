359bdf1825e475ec3c590d3a0bfa8458823d934d
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-05-10 19:05:16 +0400

    пример из лекции использования статического конструктора

---
+++ ConsoleAppPriv8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppPriv8
+{
+  class Test
+  {
+    public static readonly DateTime Readonly;
+    public readonly int Number;
+    //Статические конструкторы всегда без параметров
+    static Test()
+    {
+      Readonly = DateTime.Now;
+    }
+    //это динамический конструктор
+    public Test()
+    {
+      Number = 3;
+    }
+  }
+
+  class Program
+  {
+    public static void Main()
+    {
+      var test = new Test();
+      //сначала вызовется статический конструктор (настройка типа)
+      //и только после этого - динамический
+    }
+  }
+}
