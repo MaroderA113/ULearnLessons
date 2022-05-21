fd512f0183e7cdac8342d2ba5ddc29310e629cd9
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-05-21 19:10:51 +0400

    пример из лекции с boxing и unboxing (апкаст и даункаст)

---
+++ ConsoleAppStruct4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppStruct4
+{
+	class Program
+	{
+    struct MyStruct
+    {
+      public int field;
+    }
+
+    public static void Main()
+    {
+      MyStruct original;
+      original.field = 10;
+
+      object boxed = (object)original;
+      MyStruct unboxed = (MyStruct)boxed;
+
+      unboxed.field = 20;
+
+      Console.WriteLine(original.field);
+      Console.WriteLine(unboxed.field);
+    }
+  }
+}
