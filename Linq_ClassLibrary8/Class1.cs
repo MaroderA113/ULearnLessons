ed0517036204a848fd87598aaf7b2c71619fc8da
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-10-09 23:01:06 +0400

    Практикум по linq - группировка, частотный словарь, Lookup
    Разбор примеров из лекции

---
+++ Linq_ClassLibrary8/Class1.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace Linq_ClassLibrary8
+{
+	public class Class1
+	{
+		public static void Test1()
+		{
+			//Нередко встречается необходимость, сгруппировав элементы, преобразовать их в структуру данных для поиска группы по ключу группировки.
+			//Это можно было бы сделать с помощью такой комбинации:
+			string[] names1 = { "Pavel", "Peter", "Andrew", "Anna", "Alice", "John" };
+
+			var namesByLetter1 = new Dictionary<char, List<string>>();
+			foreach (var group in names1.GroupBy(name => name[0]))
+				namesByLetter1.Add(group.Key, group.ToList());
+
+			Assert.That(namesByLetter1['J'], Is.EquivalentTo(new[] { "John" }));
+			Assert.That(namesByLetter1['P'], Is.EquivalentTo(new[] { "Pavel", "Peter" }));
+			Assert.IsFalse(namesByLetter1.ContainsKey('Z'));
+
+
+
+			//Ровно того же эффекта можно добиться и без цикла при помощи LINQ-метода ToDictionary:
+			//IDictionary<K, V> ToDictionary(this IEnumerable<T> items, Func<T, K> keySelector, Func<T, V> valueSelector)
+			string[] names2 = { "Pavel", "Peter", "Andrew", "Anna", "Alice", "John" };
+
+			Dictionary<char, List<string>> namesByLetter2 = names2
+				.GroupBy(name => name[0])
+				.ToDictionary(group => group.Key, group => group.ToList());
+
+			Assert.That(namesByLetter2['J'], Is.EquivalentTo(new[] { "John" }));
+			Assert.That(namesByLetter2['P'], Is.EquivalentTo(new[] { "Pavel", "Peter" }));
+			Assert.IsFalse(namesByLetter2.ContainsKey('Z'));
+
+
+
+			//Но еще проще воспользоваться специальным методом ToLookup:
+			//ILookup<K, T> ToLookup(this IEnumerable<T> items, Func<T, K> keySelector)
+			//ILookup<K, V> ToLookup(this IEnumerable<T> items, Func<T, K> keySelector, Func<T, V> valueSelector)
+			string[] names3 = { "Pavel", "Peter", "Andrew", "Anna", "Alice", "John" };
+			ILookup<char, string> namesByLetter3 = names3.ToLookup(name => name[0], name => name.ToLower());
+
+			Assert.That(namesByLetter3['J'], Is.EquivalentTo(new[] { "john" }));
+			Assert.That(namesByLetter3['P'], Is.EquivalentTo(new[] { "pavel", "peter" }));
+
+			// Lookup по неизвестному ключу возвращает пустую коллекцию. 
+			//Часто это удобнее, чем поведение Dictionary, который в такой ситуации бросает исключение.
+			Assert.That(namesByLetter3['Z'], Is.Empty);
+
+		}
+
+	}
+}
---
+++ Linq_ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Linq_ConsoleApp6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var vocabulary = GetMostFrequentWords(
+				"Hello, hello, hello, how low , With the lights out, it's less dangerous Here we are now; entertain us I feel stupid and contagious", 5);
+
+			foreach (var word in vocabulary)
+				Console.WriteLine(word);
+		}
+
+		//Дан текст, нужно вывести count наиболее часто встречающихся в тексте слов вместе с их частотой.
+		//Среди слов, встречающихся одинаково часто, отдавать предпочтение лексикографически меньшим словам.
+		//Слова сравнивать регистронезависимо и выводить в нижнем регистре.
+
+		public static Tuple<string, int>[] GetMostFrequentWords(string text, int count)
+		{
+			return Regex.Split(text, @"\W+") //все слова
+						.Where(w => w != "") //слова без пустых множеств
+						.Select(w => w.ToLower()) //слова в нижнем регистре
+						.GroupBy(w => w) //группы одинаковых слов
+						.Select(g => Tuple.Create(g.Key, g.Count())) //пары ("слово","количество")
+						.OrderByDescending(t => t.Item2) //пары сортированные по количеству
+						.ThenBy(t => t.Item1) //пары сортированные по количеству и лексикографически
+						.Take(count) //взяли несколько (count) таких пар
+						.ToArray(); //IEnumerable => Array
+		}
+	}
+}
