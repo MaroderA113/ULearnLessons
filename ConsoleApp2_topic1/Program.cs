d0e2e81f8cef5ac34599570491d7546d66da4392
it.sgusev@gmail.com <it.sgusev@gmail.com>
2022-11-20 13:31:40 +0400

    Разбор примеров про очереди

---
+++ ConsoleApp2_topic1/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleApp2_topic1
+{
+	/// <summary>
+	/// Элемент очереди
+	/// </summary>
+	public class QueueItem
+		{
+		/// <summary>
+		/// Значение
+		/// </summary>	
+		public int Value { get; set; }
+		/// <summary>
+		/// Указатель на следующий
+		/// </summary>	
+		public QueueItem Next { get; set; }
+		}
+
+	/// <summary>
+	/// Очередь
+	/// </summary>
+	public class Queue
+		{
+		/// <summary>
+		/// Указатель на голову
+		/// </summary>
+		QueueItem head;
+
+		/// <summary>
+		/// Указатель на хвост
+		/// </summary>
+		QueueItem tail;
+
+		/// <summary>
+		/// Добавить элемент в очередь
+		/// </summary>
+		/// <param name="value">Добавляемый элемент</param>
+		public void Enqueue(int value)
+			{
+				if (head == null)
+					tail = head = new QueueItem { Value = value, Next = null };
+				else
+				{
+					var item = new QueueItem { Value = value, Next = null };
+					tail.Next = item;
+					tail = item;
+				}
+			}
+
+		/// <summary>
+		/// Удалить элемент из очереди
+		/// </summary>
+		/// <returns>Значение первого элемента</returns>
+		public int Dequeue()
+			{
+				if (head == null) throw new InvalidOperationException();
+				var result = head.Value;
+
+				head = head.Next;
+				if (head == null)
+					tail = null;
+				return result;
+			}
+		}
+	internal class Program
+	{ 
+		static void Main(string[] args)
+		{
+			var queue = new Queue();
+			for (int i = 0; i < 3; i++)
+			{
+				queue.Enqueue(i);
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine(queue.Dequeue());
+			}
+		}
+	}
+}
---
+++ ConsoleApp_topic1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp_topic1
+{
+	internal class Program
+	{
+		static Random rnd = new Random();
+
+		static int? GetNumber7()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (Console.KeyAvailable)
+				{
+					return rnd.Next(100);
+				}
+				Thread.Sleep(100);
+			}
+			return null;
+		}
+
+		static void Main()
+		{
+			var value = GetNumber7();
+			if (value != null)
+				Console.WriteLine(value);
+			else
+			{
+				Console.WriteLine(value.HasValue);
+				Console.WriteLine("The server was unreachable");
+			}		
+		}
+
+	}
+}
