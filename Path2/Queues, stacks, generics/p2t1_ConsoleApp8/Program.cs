188bebf832927876cffad2dc44521e3c1fe97408
it.sgusev@gmail.com <it.sgusev@gmail.com>
2023-09-16 12:34:48 +0400

    Максимум в массиве

---
+++ Path2/Queues, stacks, generics/p2t1_ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace p2t1_ConsoleApp8;
+
+internal class Program
+{
+	static T? Max<T>(T[] source) where T : IComparable
+	{
+		if (source.Length == 0)
+			return default;
+
+		T max = source[0];
+		foreach (T item in source)
+		{
+			if (item.CompareTo(max) > 0)
+				max = item;
+		}
+
+		return max;
+	}
+
+	static void Main(string[] args)
+	{
+		Console.WriteLine(Max<int>(new int[0]));
+		Console.WriteLine(Max(new[] { 3 }));
+		Console.WriteLine(Max(new[] { 3, 1, 2 }));
+		Console.WriteLine(Max(new[] { "A", "B", "C" }));
+	}
+}
