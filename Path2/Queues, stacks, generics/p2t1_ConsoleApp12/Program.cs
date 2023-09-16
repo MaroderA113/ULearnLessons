4b93ccca29f4208a5ab15c50361f88f9771b415d
it.sgusev@gmail.com <it.sgusev@gmail.com>
2023-09-16 13:53:33 +0400

    Дженерик-класс Nullable

---
+++ Path2/Queues, stacks, generics/p2t1_ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace p2t1_ConsoleApp12;
+
+internal class Program
+{
+	static Random rnd = new Random();
+
+	static int? GetNumber7()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (Console.KeyAvailable)
+				return rnd.Next(100);
+			Thread.Sleep(100);
+		}
+		return null;
+	}
+
+	static void Main7()
+	{
+		var value = GetNumber7();
+		if (value != null)
+			Console.WriteLine(value);
+		else
+			Console.WriteLine("Error");
+	}
+
+	static void Main(string[] args)
+	{
+		Main7();
+	}
+}
