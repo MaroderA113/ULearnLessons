0e29435bf6a45a31cb7b7a4ef7637ec0721880f1
it.sgusev@gmail.com <it.sgusev@gmail.com>
2023-10-08 18:38:56 +0400

    Делегаты

---
+++ Path2/Delegates/p2t4_ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace p2t4_ConsoleApp1;
+
+public class Program
+{
+
+	//Метод Sort делегирует интерфейсу IComparer функциональность
+	//по сравнению элементов массива
+	public static void SortStrings(string[] array, IComparer<string> comparer)
+	{
+		for (int i = array.Length - 1; i > 0; i--)
+			for (int j = 1; j <= i; j++)
+			{
+				var element1 = array[j - 1];
+				var element2 = array[j];
+				if (comparer.Compare(element1, element2) > 0)
+				{
+					var temporary = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temporary;
+				}
+			}
+	}
+
+	//Но для обеспечения делегирования приходится писать 
+	//слишком много инфраструктурного кода: объявлять класс,
+	//реализовывать интерфейс. Задача: сократить объем инфраструктурного кода
+	class StringLengthComparer : IComparer<string>
+	{
+		public int Compare(string x, string y)
+		{
+			return x.Length.CompareTo(y.Length);
+		}
+	}
+
+	class StringComparer : IComparer<string>
+	{
+
+		public int Compare(string x, string y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+
+	static void Main()
+	{
+		var strings = new[] { "A", "B", "AA" };
+		SortStrings(strings, new StringComparer());
+		SortStrings(strings, new StringLengthComparer());
+	}
+}
---
+++ Path2/Delegates/p2t4_ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace p2t4_ConsoleApp2;
+
+//Здесь объявляется тип указателей на функции, принимающие
+//две строки и возвращающие int.
+//Делегат - это не член класса! Это тип данных.
+public delegate int StringComparer(string x, string y);
+
+public class Program
+{
+	//Этот метод принимает указатель на соответствующие функции
+	public static void SortStrings(string[] array, StringComparer comparer)
+	{
+		for (int i = array.Length - 1; i > 0; i--)
+			for (int j = 1; j <= i; j++)
+			{
+				var element1 = array[j - 1];
+				var element2 = array[j];
+				//раз это указатель на функцию, значит, эту функцию можно вызвать
+				if (comparer(element1, element2) > 0)
+				{
+					var temporary = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temporary;
+				}
+			}
+	}
+
+	static int CompareStringLength(string x, string y)
+	{
+		return x.Length.CompareTo(y.Length);
+	}
+
+	static void Main()
+	{
+		var strings = new[] { "A", "B", "AA" };
+		//Здесь создается указатель на метод CompareStringLength
+		var lengthComparer =
+			new StringComparer(CompareStringLength);
+
+		//... и передается в метод SortStrings
+		SortStrings(strings, lengthComparer);
+
+		//Можно писать так. Компилятор сам догадается, что вы хотели создать 
+		//указатель на метод, а не вызвать его.
+		SortStrings(strings, CompareStringLength);
+	}
+}
