0f2248873838b2489dab1db0b6fcc9a2784158f0
it.sgusev@gmail.com <it.sgusev@gmail.com>
2023-10-15 22:45:43 +0400

    Func и Action. Анонимные делегаты

---
+++ Path2/Delegates/p2t4_ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace p2t4_ConsoleApp6;
+
+public class Program
+{
+	public static void Sort<T>(T[] array, Func<T, T, int> comparer)
+	{
+		for (int i = array.Length - 1; i > 0; i--)
+			for (int j = 1; j <= i; j++)
+			{
+				var element1 = array[j - 1];
+				var element2 = array[j];
+				if (comparer(element1, element2) > 0)
+				{
+					var temporary = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temporary;
+				}
+			}
+	}
+
+	static int CompareStringLength(string x, string y)
+	{
+		return x.Length.CompareTo(y.Length);
+	}
+
+	class Comparer
+	{
+		public bool Descending { get; set; }
+		public int CompareStrings(string x, string y)
+		{
+			return x.CompareTo(y) * (Descending ? -1 : 1);
+		}
+	}
+
+	static void Main()
+	{
+		var strings = new[] { "A", "B", "AA" };
+		Sort(strings, CompareStringLength);
+
+		var obj = new Comparer { Descending = true };
+		Sort(strings, obj.CompareStrings);
+	}
+}
---
+++ Path2/Delegates/p2t4_ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace p2t4_ConsoleApp7;
+
+public class Program
+{
+	public static void Sort<T>(T[] array, Func<T, T, int> comparer)
+	{
+		for (int i = array.Length - 1; i > 0; i--)
+			for (int j = 1; j <= i; j++)
+			{
+				var element1 = array[j - 1];
+				var element2 = array[j];
+				if (comparer(element1, element2) > 0)
+				{
+					var temporary = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temporary;
+				}
+			}
+	}
+
+	static void Main()
+	{
+		var strings = new[] { "A", "B", "AA" };
+		//Анонимный делегат позволяет не писать метод, а определить его по месту использования
+		//Компилятор напишет метод за вас и сам придумает ему имя и тип возвращаемого значения
+		//Это позволяет еще больше сократить объем инфраструктурного кода.
+		Sort(strings, delegate (string x, string y)
+		{
+			return x.Length.CompareTo(y.Length);
+		});
+	}
+}
